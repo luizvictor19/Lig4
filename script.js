e9e951795a08227c047b48979259e55947fa918b
Luiz Victor Bispo Oliveira <luizvictorred@gmail.com>
2021-06-07 20:33:46 -0300

    swap

--- script.js
+++ script.js
@@ -5,6 +5,27 @@
 
 
 // Luiz //
+let jogada = 'vermelho'
+let torre = document.createElement('div')
+torre.className = 'torre-teste'
+document.body.appendChild(torre)
+let disco = document.createElement('div')
+torre.onclick = function() {
+    swapPlayer()
+}
+function swapPlayer(){
+    if(jogada === 'vermelho'){
+        disco.classList.remove('disco-black')
+        disco.classList.add('disco-red')
+        torre.appendChild(disco)
+        jogada = 'preto'
+    }else if(jogada === 'preto'){
+        disco.classList.remove('disco-red')
+        disco.classList.add('disco-black')
+        torre.appendChild(disco)
+        jogada = 'vermelho'
+    }
+}
 
 
 // Lucas //
