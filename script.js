7fde236a8bac8eaed88f3e111d6d18ebeb873e53
Luiz Victor Bispo Oliveira <luizvictorred@gmail.com>
2021-06-07 22:26:28 -0300

    Player Swap finalizado

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ container.id = "container";
 body.appendChild(container);
 
 const board = [
-    "BBBBBBB",
-    "BBBBBBB",
-    "BBBBBBB",
-    "BBBBBBB",
-    "BBBBBBB",
-    "BBBBBBB",
+    "BBBBBB",
+    "BBBBBB",
+    "BBBBBB",
+    "BBBBBB",
+    "BBBBBB",
+    "BBBBBB",
+    "BBBBBB"
 ];
 
 
@@ -21,15 +22,15 @@ const board = [
 //criar um array com todas as posições das div's:
 const criarBoard = () => {
     for (let i = 0; i < board.length; i++) {
-        let linha = document.createElement('div');
-        linha.classList = "linha"
-        linha.dataset.index = i
-        container.appendChild(linha);
+        let coluna = document.createElement('div');
+        coluna.classList = "coluna"
+        coluna.dataset.index = i
+        container.appendChild(coluna);
         for (let j = 0; j < board[i].length; j++) {
-            let coluna = document.createElement('div');
-            coluna.classList = "coluna"
-            coluna.dataset.index = j
-            linha.appendChild(coluna);
+            let linha = document.createElement('div');
+            linha.classList = "linha"
+            linha.dataset.index = j
+            coluna.appendChild(linha);
         }
     }
 }
@@ -37,27 +38,56 @@ const criarBoard = () => {
 criarBoard()
 
 // Luiz //
+document.getElementById("container").addEventListener("click", someFunction);
+let torre = ''
+function someFunction(event) {
+  torre = event.target
+//   torre = document.getElementsByClassName(torre)
+} 
+
 let jogada = 'vermelho'
-let torre = document.createElement('div')
-torre.className = 'torre-teste'
-document.body.appendChild(torre)
-let disco = document.createElement('div')
-torre.onclick = function() {
-    swapPlayer()
-}
-function swapPlayer(){
-    if(jogada === 'vermelho'){
-        disco.classList.remove('disco-black')
-        disco.classList.add('disco-red')
-        torre.appendChild(disco)
-        jogada = 'preto'
-    }else if(jogada === 'preto'){
-        disco.classList.remove('disco-red')
-        disco.classList.add('disco-black')
-        torre.appendChild(disco)
-        jogada = 'vermelho'
+
+function swapPlayer(event){
+    torre = event.target
+    let teste = event.currentTarget.children
+
+
+    for(let i = 0; i < teste.length; i++){
+        if(!teste[i].hasChildNodes()){
+            if(jogada === 'vermelho'){
+                let disco = document.createElement('div')
+                disco.classList.remove('disco-black')
+                disco.classList.add('disco-red')
+                // torre.appendChild(disco)
+                teste[i].appendChild(disco)
+                jogada = 'preto'
+                break
+            }else if(jogada === 'preto'){
+                let disco = document.createElement('div')
+                disco.classList.remove('disco-red')
+                disco.classList.add('disco-black')
+                // torre.appendChild(disco)
+                teste[i].appendChild(disco)
+                jogada = 'vermelho'
+                break
+            }
+            
+        }
     }
+
 }
 
+let array = document.querySelectorAll('.coluna')
+
+for(let i = 0; i < array.length; i++){
+    array[i].addEventListener("click", swapPlayer)
+}
+
+// function addDisco() {
+//     for(let i = 0; i < array.length; i++){
+//         console.log(array[i].children[i])
+//     }
+// }
+// addDisco()
 
 // Lucas //
